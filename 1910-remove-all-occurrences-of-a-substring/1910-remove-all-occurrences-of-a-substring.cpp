93d1960efa2904d2fb98b7270034ad2096fe123d
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-02-11 11:37:51 +0200

    Time: 3 ms (32.43%), Space: 9.4 MB (68.15%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        size_t pos;
+        while ((pos = s.find(part)) != string::npos) {  // Find the first occurrence
+            s.erase(pos, part.length());  // Remove `part`
+        }
+        return s;
+    }
+};
