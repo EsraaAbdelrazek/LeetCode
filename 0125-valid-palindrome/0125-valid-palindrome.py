0a22cc20efdc05dadf1b364a7b80289ccbc5c062
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-02-21 13:40:09 +0200

    Time: 7 ms (81.13%), Space: 18.2 MB (57.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,8 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        newstr = "" 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newstr = '' 
         for c in s : 
-            if c.isalnum() : 
-                newstr += c.lower ()
+            if c.isalnum () : 
+                newstr += c.lower()
 
-        return newstr == newstr [::-1] 
-        
-
-        
+        return newstr[:] == newstr[::-1]        
