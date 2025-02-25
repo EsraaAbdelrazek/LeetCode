65af62b5aa6f559021539d3c86bc637fccf5e82b
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-02-25 23:37:05 +0200

    Time: 0 ms (100%), Space: 18.9 MB (34.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,14 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}  # Dictionary to store number and its index
-        for i, num in enumerate(nums):
-            complement = target - num
+        num_map = {} 
+        length = len(nums)
+        
+        for i in range(length):
+            complement = target - nums[i]
             if complement in num_map:
-                return [num_map[complement], i]  # Return indices of the two numbers
-            num_map[num] = i  # Store the number and its index
-        return []  # This case won't occur as per the problem statement    
+                return [num_map[complement], i] 
+            num_map[nums[i]] = i 
+        
+        return []
