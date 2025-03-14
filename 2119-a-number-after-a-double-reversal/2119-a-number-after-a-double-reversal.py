5b10833c6e023c2124ca17fdd95c588bf4d03e2f
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-03-14 22:38:00 +0200

    0 ms (100%) 17.7 MB (82.32%) 03-14-2025 2119-a-number-after-a-double-reversal Easy .py

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
@@ -1,17 +1,18 @@
 class Solution:
     def isSameAfterReversals(self, num: int) -> bool:
-        num_str = str (num)
-        if num_str [-1] == '0' and len (num_str) == 1 : 
-            return True 
+              return str(num) == str(int(str(num)[::-1]))[::-1]
+        # num_str = str (num)
+        # if num_str [-1] == '0' and len (num_str) == 1 : 
+        #     return True 
 
-        if num_str [-1] == '0' and len (num_str) != 1 : 
-            return False 
-        reversed1 = num_str[::-1] 
-        reversed2 = reversed1 [::-1] 
+        # if num_str [-1] == '0' and len (num_str) != 1 : 
+        #     return False 
+        # reversed1 = num_str[::-1] 
+        # reversed2 = reversed1 [::-1] 
 
-        if reversed2 == num_str : 
-                return True 
-        else : 
-                return False 
+        # if reversed2 == num_str : 
+        #         return True 
+        # else : 
+        #         return False 
 
         
