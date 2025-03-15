1682a7f8f1ceb4d362bc24261c38fee928393ac6
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-03-15 16:31:20 +0200

    0 ms (100%) 17.6 MB (82.65%) 03-15-2025 0198-house-robber Medium .py

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -6,11 +6,11 @@ class Solution:
             if len(nums) == 1:
                 return nums[0]
 
-            prev2, prev1 = 0, 0  # Represents dp[i-2] and dp[i-1]
+            prev2, prev1 = 0, 0 
             
             for num in nums:
-                new_rob = max(prev1, num + prev2)  # max of skipping or robbing current house
-                prev2, prev1 = prev1, new_rob  # Shift values forward
+                new_rob = max(prev1, num + prev2) 
+                prev2, prev1 = prev1, new_rob 
             
             return prev1  # The maximum money that can be robbed
 
