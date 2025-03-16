7f2ec68951f9e03e971a2e26d9d3c8e2a882ecd0
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-03-16 11:40:26 +0200

    4 ms (46.77%) 19.8 MB (22.37%) 03-16-2025 0136-single-number Easy .py

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        x = Counter (nums) 
+        for i, j in x.items()  : 
+            if j == 1: 
+                return i 
+
+     
