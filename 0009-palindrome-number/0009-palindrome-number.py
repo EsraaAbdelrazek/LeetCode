9981b0a6ff00d40c63aadef1c1133cdbf0c933f4
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-03-16 11:47:04 +0200

    1 ms (96.42%) 12.5 MB (11.11%) 03-16-2025 0009-palindrome-number Easy .py

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
+    
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+    
+        reversed_half = 0
+        while x > reversed_half:
+        # Extract the last digit and add it to the reversed half
+            reversed_half = reversed_half * 10 + x % 10
+        # Remove the last digit from x
+            x //= 10
+    
+    # Check if the original number is equal to the reversed half
+    # For odd-length numbers, we can ignore the middle digit by reversed_half // 10
+        return x == reversed_half or x == reversed_half // 10
