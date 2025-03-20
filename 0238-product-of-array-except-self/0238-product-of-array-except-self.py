9cb42413bce282b158f767249c2aa5f4a9afe37b
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-03-20 23:01:55 +0200

    18 ms (96.96%) 23.3 MB (54.43%) 03-20-2025 0238-product-of-array-except-self Medium .py

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,37 @@
-from typing import List
+# from typing import List
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         answer = [1] * n
+
+#         # Step 1: Calculate prefix products
+#         prefix = 1
+#         for i in range(n):
+#             answer[i] = prefix
+#             prefix *= nums[i]
+
+#         # Step 2: Calculate suffix products and multiply
+#         suffix = 1
+#         for i in range(n - 1, -1, -1):
+#             answer[i] *= suffix
+#             suffix *= nums[i]
+
+#         return answer
+
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        answer = [1] * n
+        res = [1] * n
+        prefix = suffix = 1
 
-        # Step 1: Calculate prefix products
-        prefix = 1
         for i in range(n):
-            answer[i] = prefix
+            res[i] *= prefix
             prefix *= nums[i]
-
-        # Step 2: Calculate suffix products and multiply
-        suffix = 1
+        
         for i in range(n - 1, -1, -1):
-            answer[i] *= suffix
+            res[i] *= suffix
             suffix *= nums[i]
-
-        return answer
+        
+        return res
