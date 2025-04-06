4719226852d1a70205c1aa32dd8a5508952f0e4c
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-04-06 02:47:03 +0200

    Time: 3 ms (63.76%), Space: 18.6 MB (72.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,20 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        n = len(nums)
-        expected_sum = n * (n + 1) // 2
-        actual_sum = sum(nums)
-        return expected_sum - actual_sum
+        # n = len(nums)
+        # expected_sum = n * (n + 1) // 2
+        # actual_sum = sum(nums)
+        # return expected_sum - actual_sum
+        
+            n = len(nums)
+            xor_result = n  # Start with n because the range is [0, n]
+            
+            for i in range(n):
+                xor_result ^= i ^ nums[i]  # XOR current index and corresponding number in array
+                
+            return xor_result
+
+
+
 
         
