d737d1e1ff3ab2c66c0dab19c5297df3ac4fc292
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-04-14 20:15:29 +0200

    Time: 5 ms (22.02%), Space: 20.9 MB (13.5%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,31 @@
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> List[int]:
+      
+        # xor = 0
+        # for num in nums:
+        #     xor ^= num
+
+        # # Find rightmost set bit
+        # diff_bit = xor & -xor
+
+        # a = 0
+        # b = 0
+        # for num in nums:
+        #     if num & diff_bit:
+        #         a ^= num
+        #     else:
+        #         b ^= num
+
+        # return [a, b]
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        hashmap = Counter(nums)
+        ans = []
+        print(hashmap)
+        for key, value in hashmap.items():
+            if value == 1:
+                ans.append(key)
+        print(ans)
+        return ans
+            
