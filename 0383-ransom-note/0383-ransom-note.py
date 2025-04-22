f6b853efba7ae9b520aa615b2fccc2c3f92de0af
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-04-22 13:02:02 +0200

    Time: 29 ms (14.93%), Space: 18.2 MB (13.88%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,5 +1,6 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-
+        print ("ransomNote" ,Counter (ransomNote) )
+        print ("magazine" ,Counter (magazine) )
         return Counter (ransomNote) <= Counter (magazine)
         
