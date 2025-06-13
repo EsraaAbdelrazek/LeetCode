40a8424b07ea9c3172980db40fa10963178de308
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-06-13 14:37:14 +0300

    Time: 274 ms (95.55%), Space: 32.7 MB (40.08%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.py
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.py
@@ -1,16 +1,16 @@
 class Solution:
     def minimizeMax(self, nums: list[int], p: int) -> int:
-        # Step 1: Sort the array
+     
         nums.sort()
         n = len(nums)
 
         low, high = 0, nums[-1] - nums[0]
 
-        # Step 3: Binary search
+      
         while low < high:
             mid = (low + high) // 2
 
-            # Step 4: Try to form pairs
+            
             count = 0
             i = 1
             while i < n and count < p:
@@ -20,11 +20,11 @@ class Solution:
                 else:
                     i += 1
 
-            # Binary search update
+     
             if count >= p:
                 high = mid
             else:
                 low = mid + 1
 
-        # Step 5: Return result
+   
         return low
