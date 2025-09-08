32ca3429c9f2f80c3da26b536fe39caabb9eb496
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-09-08 23:39:26 +0300

    Time: 3 ms (11.6%), Space: 17.7 MB (83.35%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -1,9 +1,14 @@
-class Solution(object):
-    def getNoZeroIntegers(self, n):
-        def check(x):
-            return '0' not in str(x)
+# class Solution(object):
+#     def getNoZeroIntegers(self, n):
+#         def check(x):
+#             return '0' not in str(x)
 
-        for i in range(1, n):
-            j = n - i
-            if check(i) and check(j):
-                return [i, j]
+#         for i in range(1, n):
+#             j = n - i
+#             if check(i) and check(j):
+#                 return [i, j]
+
+
+class Solution(object): 
+    def getNoZeroIntegers(self, n): 
+        return next([i, n - i] for i in range(1, n) if '0' not in str(i) and '0' not in str(n - i))
