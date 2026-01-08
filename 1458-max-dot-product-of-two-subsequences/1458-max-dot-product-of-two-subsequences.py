2c481abf913cb3b6fc147b2988102ad970b86ea2
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2026-01-08 09:47:00 +0200

    Time: 593 ms (7.04%), Space: 109.6 MB (5.53%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -3,27 +3,27 @@ class Solution:
         
         memo = {}
 
-        # max dot product of two subsequences starting at i,j
+       
         def dp(i,j):
 
             if i == len(nums1) or j == len(nums2):
-                return float("-inf") # if we are passed the boundary, dont pick anything from there.
+                return float("-inf") 
 
             if (i,j) in memo:
                 return memo[(i,j)]
 
             take = nums1[i] * nums2[j]
             res = max(
-            # take i,j. move forward
+         
                 take + dp(i+1, j+1),
 
-            # take this subsequence and just end.
+    
                 take,
 
-            # skip i: i+1,j
+           
                 dp(i+1,j),
 
-            # skip j: i,j+1
+          
                 dp(i,j+1),
             )
 
